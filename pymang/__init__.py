103317039cd54f9cfa50270df35d8c2e8987cd09
Chris46672 <68782340+Chris46672@users.noreply.github.com>
2021-03-02 01:37:49 +0300

    Add files via upload

--- pymang/__init__.py
+++ pymang/__init__.py
@@ -3,34 +3,59 @@ import shutil
 import sys
 import glob
 
+'''
+A file management library
+'''
+__all__ = ['Class']
 class mg:
-   def delete(self,file):
-       os.remove(file)
-       print(str(file)+" was removed from system")
+   #delete file
+    def delete(self,file):
+        '''This:
+              delete file
+              *arg
+        '''
+        os.remove(file)
+        print(str(file)+" was removed from system")
+   
+   #transfer file
+    def transfer(self,file1,file2):
+        '''transfer file
+        used to transfer file
+        '''
+        shutil.move(file1,file2)
+        print(str(file1)+" was moved to "+str(file2))
+    #make directory
+    def mkdiry(self,name):
+        '''Use:
+              make a directory
+        '''
+        os.mkdiry(name)
+        print(f"A new directory named '{name}' was created")
 
-   def transfer(self,file1,file2):
-       shutil.move(file1,file2)
-       print(str(file1)+" was moved to "+str(file2))
+    #list 
+    def list():
+        '''list all directories'''
+        glob.glob("*")
+    
+    #copy file
+    def copy(self,file1,file2):
+        '''copy file'''
+        shutil.copy(file1,file2)
+        print(f"{file1} was copied to {file2}")
+    
+    #new file
+    def newf(self,file,words):
+        '''create new file'''
+        f=open(file,"w+")
+        f.write(words)
+        f.close()
+    #exit
+    def exit(self):
+        '''exit system'''
+        sys;exit()
 
-   def mkdiry(self,name):
-       os.mkdiry(name)
-       print(f"A new directory named '{name}' was created")
-
-   def list():
-       glob.glob("*")
-
-   def copy(self,file1,file2):
-       shutil.copy(file1,file2)
-       print(f"{file1} was copied to {file2}")
-
-   def newf(self,file,words):
-       f=open(file,"w+")
-       f.write(words)
-       f.close()
-
-   def exit(self):
-       sys;exit()
-
-   def rmdiry(self,name):
-       os.rmdir(name)
-       print(f"{name} was removed")
+    #remove directory
+    def rmdiry(self,name):
+        '''delete the directory'''
+        os.rmdir(name)
+        print(f"{name} was removed")
